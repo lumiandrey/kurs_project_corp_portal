7ceed6974d84788d9a42e4f41b1609deccace8a0
DariaSSemikina <dashulya_plyus@mail.ru>
2016-04-06 19:49:10 +0300

    Description

---
+++ RESTfulAPIKorpPortal/src/main/java/by/bsuir/ief/rest/controllers/sdfgh.java
@@ -0,0 +1,10 @@
+package by.bsuir.ief.rest.controllers;
+
+/**
+ * Created by Даша on 06.04.2016.
+ */
+public class sdfgh {
+    public static void main(String[] args) {
+        System.out.println("ky!");
+    }
+}
