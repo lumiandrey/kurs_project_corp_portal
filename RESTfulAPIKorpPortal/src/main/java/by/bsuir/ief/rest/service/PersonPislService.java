c746c9d3453e1a732d16ac60814eca6d405f2a00
lumiandrey <lumiandrey@live.com>
2016-04-15 23:02:19 +0300

    Доработка класса "оболочки" сервиса для работы с сущностью Персона лабы ПИСЛ
    Добавлена поддержка извлечения всех пользователей и извлечения по
    уникальному id исключения на этом уровне не обрабатываются а
    транслируются в вызываемые методы

--- RESTfulAPIKorpPortal/src/main/java/by/bsuir/ief/rest/service/PersonPislService.java
+++ RESTfulAPIKorpPortal/src/main/java/by/bsuir/ief/rest/service/PersonPislService.java
@@ -2,9 +2,12 @@ package by.bsuir.ief.rest.service;
 
 import by.bsuir.ief.rest.dao.pisl.PersonPislDAO;
 import by.bsuir.ief.rest.dao.pisl.PersonPislDAOImpl;
+import by.bsuir.ief.rest.model.pisl.PersonPisl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 /**
  * Created by andrey on 12.04.2016.
@@ -15,4 +18,21 @@ public class PersonPislService {
     @Qualifier("personPislDAOImpl1")
     @Autowired
     private PersonPislDAO personPislDAOImpl1;
+
+    /**
+     *
+     * @return
+     * @throws Exception
+     */
+    public List<PersonPisl> getAllPerson() throws Exception {
+
+        return personPislDAOImpl1.getEntityList();
+    }
+
+    public PersonPisl getPersonByID (int id) throws Exception {
+        return personPislDAOImpl1.getEntityById(id);
+    }
+
+
+
 }
