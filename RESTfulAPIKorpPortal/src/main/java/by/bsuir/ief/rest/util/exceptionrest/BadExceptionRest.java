8cb0e91276b5091dc137115501dcd91e1424205d
lumiandrey <lumiandrey@live.com>
2016-04-17 13:42:53 +0300

    переименование пакета exception в exceptionrest

--- RESTfulAPIKorpPortal/src/main/java/by/bsuir/ief/rest/util/exceptionrest/BadExceptionRest.java
+++ RESTfulAPIKorpPortal/src/main/java/by/bsuir/ief/rest/util/exceptionrest/BadExceptionRest.java
@@ -1,11 +1,11 @@
-package by.bsuir.ief.rest.util.exception;
+package by.bsuir.ief.rest.util.exceptionrest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value= HttpStatus.BAD_REQUEST, reason="Very bad")  // 404
-public class BadException extends RuntimeException {
-    public BadException() {
-        super("Smth is bad");
+public class BadExceptionRest extends RuntimeException {
+    public BadExceptionRest(String messages) {
+        super(messages);
     }
 }
