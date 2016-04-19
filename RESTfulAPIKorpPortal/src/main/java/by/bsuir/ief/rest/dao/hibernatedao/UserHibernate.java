cfafd3a8e4e2dfc9fd6f6d4ccd5e7da34e1e62d2
DariaSSemikina <dashulya_plyus@mail.ru>
2016-04-19 16:32:40 +0300

    Класс Дао для User
    крут-операции с базой данных

--- RESTfulAPIKorpPortal/src/main/java/by/bsuir/ief/rest/dao/hibernatedao/UserHibernate.java
+++ RESTfulAPIKorpPortal/src/main/java/by/bsuir/ief/rest/dao/hibernatedao/UserHibernate.java
@@ -1,20 +1,28 @@
 package by.bsuir.ief.rest.dao.hibernatedao;
 
 import by.bsuir.ief.rest.dao.UserDAO;
+import by.bsuir.ief.rest.dao.pisl.PersonPislDAOImpl;
 import by.bsuir.ief.rest.model.entity.User;
+import by.bsuir.ief.rest.model.exception.notfoundexception.EntityNotFoundByIdException;
+import org.apache.log4j.Logger;
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 /**
  * Created by andrey on 05.04.2016.
  */
-@Repository
+@Transactional
+@Repository("userHibernate")
+
 public class UserHibernate implements UserDAO {
+
 
     @Qualifier("sessionFactory")
     @Autowired
@@ -28,24 +36,57 @@ public class UserHibernate implements UserDAO {
         return sessionFactory.getCurrentSession();
     }
 
+
+    private final String findByIdUser = "from User where idUser = :idUser";
+    static final Logger logger = Logger.getLogger(PersonPislDAOImpl.class);
+
     @Override
-    public void create(User createUser) {
+    public User create(User createUser) {
         currentSession().save(createUser);
+        logger.info("Add entity to db: "+createUser);
+        return createUser;
     }
 
     @Override
     public List<User> readAll() {
-        return null;
+
+        List<User> userList = currentSession().createCriteria(User.class).list();
+        logger.info("Get entitys from db:" + userList);
+        return userList;
+    }
+
+
+    @Override
+    public User read(int id) throws EntityNotFoundByIdException {
+
+        Query query = currentSession().createQuery(findByIdUser);
+        query.setParameter("idUser",new Integer(id));
+        User getUser = (User) query.uniqueResult();
+        if(getUser == null )
+            throw new EntityNotFoundByIdException(id,User.class.getName());
+        logger.info("Get by id: " +id + " entity: "+getUser);
+        return getUser;
+    }
+
+
+    @Override
+    public boolean delete(User deleteUser) throws EntityNotFoundByIdException {
+        Query query = currentSession().createQuery(findByIdUser);
+        query.setParameter("idUser",new Integer(deleteUser.getIdUser()));
+        User pisls = (User) query.uniqueResult();
+        if(pisls == null) {
+            throw new EntityNotFoundByIdException(deleteUser.getIdUser(), User.class.getName());
+        }
+        logger.info("Delete from db by id: " +deleteUser.getIdUser()+" entity: " + pisls);
+        currentSession().delete(pisls);
+        return true;
     }
 
     @Override
-    public User read(int id) {
-        return null;
-    }
-
-    @Override
-    public void delete(User deleteUser) {
-
+    public User update(User updateUser) {
+        currentSession().update(updateUser);
+        logger.info("Update entity: "+updateUser);
+        return updateUser;
     }
 
     @Override
