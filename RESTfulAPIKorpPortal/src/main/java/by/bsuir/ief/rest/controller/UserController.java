2dd5db3e10f32b49bcbfd334cf81de11cc16f462
lumiandrey <lumiandrey@live.com>
2016-04-21 21:00:17 +0300

    Добавление метода(пустой) для запроса на авторизацию (всегда возвращает OK) по URI (/users/autorized)

--- RESTfulAPIKorpPortal/src/main/java/by/bsuir/ief/rest/controller/UserController.java
+++ RESTfulAPIKorpPortal/src/main/java/by/bsuir/ief/rest/controller/UserController.java
@@ -1,13 +1,11 @@
 package by.bsuir.ief.rest.controller;
 
+import by.bsuir.ief.rest.model.entity.User;
 import by.bsuir.ief.rest.model.service.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by Андрей on 07.04.2016.
@@ -22,9 +20,15 @@ public class UserController {
     private UsersService usersService;
 
     @RequestMapping(value = "/test", method = RequestMethod.GET)
-    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @ResponseStatus(HttpStatus.OK)
     public void test()
     {
         System.out.println("Run test");
+    }
+
+    @RequestMapping(value = "/autorized", method = RequestMethod.POST)
+    public void autorization(@RequestBody User user)
+    {
+
     }
 }
