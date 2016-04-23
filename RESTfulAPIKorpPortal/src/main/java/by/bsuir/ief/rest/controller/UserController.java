eded7fb403ea16fdd7c09406d9027963b0c9f11a
lumiandrey <lumiandrey@live.com>
2016-04-23 21:41:51 +0300

    обновление функциональности UserController
    Добавлена возможность запросов на авторизацию и регистрацию, удалён
    метод test, который проверял доступность этого контроллера для запросов

--- RESTfulAPIKorpPortal/src/main/java/by/bsuir/ief/rest/controller/UserController.java
+++ RESTfulAPIKorpPortal/src/main/java/by/bsuir/ief/rest/controller/UserController.java
@@ -1,7 +1,11 @@
 package by.bsuir.ief.rest.controller;
 
 import by.bsuir.ief.rest.model.entity.User;
+import by.bsuir.ief.rest.model.exception.notfoundexception.EntityNotFoundByParametrsException;
 import by.bsuir.ief.rest.model.service.UsersService;
+import by.bsuir.ief.rest.util.exceptionrest.BadExceptionRest;
+import by.bsuir.ief.rest.util.exceptionrest.EntityNotFoundExceptionRest;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
@@ -14,21 +18,34 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/users")
 public class UserController {
 
-
     @Qualifier("usersService")
     @Autowired
     private UsersService usersService;
 
-    @RequestMapping(value = "/test", method = RequestMethod.GET)
-    @ResponseStatus(HttpStatus.OK)
-    public void test()
-    {
-        System.out.println("Run test");
-    }
+    static final Logger logger = Logger.getLogger(UserController.class);
 
     @RequestMapping(value = "/autorized", method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.OK)
     public void autorization(@RequestBody User user)
     {
-
+        try {
+            usersService.autorizen(user);
+        } catch (EntityNotFoundByParametrsException e) {
+            logger.warn(e);
+            throw new EntityNotFoundExceptionRest(e.toString());
+        }
     }
+
+    @RequestMapping(value = "/registration", method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.OK)
+    public void registration(@RequestBody User user)
+    {
+        try {
+            usersService.registration(user);
+        } catch (Exception e) {
+            logger.warn(e);
+            throw new BadExceptionRest(e.toString());
+        }
+    }
+
 }
