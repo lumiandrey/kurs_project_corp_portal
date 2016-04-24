07623a570a518b6691796d97ff72da5b8eba5057
lumiandrey <lumiandrey@live.com>
2016-04-24 09:59:47 +0300

    изменение типов возвращаемых значений

--- RESTfulAPIKorpPortal/src/main/java/by/bsuir/ief/rest/dao/PersonDAO.java
+++ RESTfulAPIKorpPortal/src/main/java/by/bsuir/ief/rest/dao/PersonDAO.java
@@ -8,9 +8,9 @@ import java.util.List;
  * Created by andrey on 05.04.2016.
  */
 public interface PersonDAO {
-    void create(Person createUser) throws Exception;
+    Person create(Person createPerson) throws Exception;
     List read() throws Exception;
     Person read(int id) throws Exception;
     void delete(int id) throws Exception;
-    void update(Person person);
+    Person update(Person person);
 }
