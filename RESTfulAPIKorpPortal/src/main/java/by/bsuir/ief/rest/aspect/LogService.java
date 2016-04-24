3565e5ccf32e5bf7e1271038a57d092dc4babb42
lumiandrey <lumiandrey@live.com>
2016-04-24 20:25:20 +0300

    Создание класса Аспекта, для логированния

---
+++ RESTfulAPIKorpPortal/src/main/java/by/bsuir/ief/rest/aspect/LogService.java
@@ -0,0 +1,66 @@
+package by.bsuir.ief.rest.aspect;
+
+import org.apache.log4j.Logger;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by andrey on 24.04.2016.
+ */
+@Component
+@Aspect
+public class LogService {
+
+    private static Logger logger = Logger.getLogger(LogService.class);
+
+    @Pointcut("execution(* *(..)) && within(by.bsuir.ief.rest.model.service.*)")
+    private void serviceMethods() {
+    };
+
+    @Pointcut("execution(* *(..)) && within(by.bsuir.ief.rest.dao.pisl.*)")
+    private void daoMethods() {
+    };
+
+    @Around("serviceMethods()")
+    public Object logActionService(ProceedingJoinPoint joinpoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        logger.info("method | " +joinpoint.getSignature().toShortString());
+        System.out.println("method begin: " + joinpoint.getSignature().toShortString() + " >>");
+        for (Object object : joinpoint.getArgs()) {
+            System.out.println("Param : " + object);
+            logger.info("Param : " + object);
+        }
+        Object object = null;
+        try {
+            object = joinpoint.proceed();
+        } catch (Throwable e) {
+            throw e;
+        }
+        long time = System.currentTimeMillis() - start;
+        System.out.println("method end: " + joinpoint.getSignature().toShortString() + ", time=" + time + " ms <<");
+        return object;
+    }
+
+    @Around("daoMethods()")
+    public Object logActionDao(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        //logger.info(joinpoint.getSignature().toShortString());
+        System.out.println("method begin: " + joinPoint.getSignature().toString() + " >>");
+        for (Object object : joinPoint.getArgs()) {
+            System.out.println("Param : " + object);
+            logger.info("Param : " + object);
+        }
+        Object o = null;
+        try {
+            o = joinPoint.proceed();
+        } catch (Throwable e) {
+            throw e;
+        }
+        long time = System.currentTimeMillis() - start;
+        System.out.println("method end: " + joinPoint.getSignature().toString() + ", time=" + time + " ms <<");
+        return o;
+    }
+}
