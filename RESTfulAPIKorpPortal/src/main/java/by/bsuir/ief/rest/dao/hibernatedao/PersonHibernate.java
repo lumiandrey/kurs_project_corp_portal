90655b81f4a1a30d23d043156f66a9c5d0ea9799
lumiandrey <lumiandrey@live.com>
2016-04-24 09:58:27 +0300

    переработка доступа к данным
    обновление доступа к данным через диспетчер транзакций

--- RESTfulAPIKorpPortal/src/main/java/by/bsuir/ief/rest/dao/hibernatedao/PersonHibernate.java
+++ RESTfulAPIKorpPortal/src/main/java/by/bsuir/ief/rest/dao/hibernatedao/PersonHibernate.java
@@ -2,6 +2,7 @@ package by.bsuir.ief.rest.dao.hibernatedao;
 
 import by.bsuir.ief.rest.dao.PersonDAO;
 import by.bsuir.ief.rest.model.entity.Person;
+import org.apache.log4j.Logger;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -18,6 +19,8 @@ import java.util.List;
 @Transactional
 public class PersonHibernate implements PersonDAO {
 
+    static final Logger logger = Logger.getLogger(PersonHibernate.class);
+
     @Autowired
     private SessionFactory sessionFactory;
 
@@ -27,58 +30,75 @@ public class PersonHibernate implements PersonDAO {
     public PersonHibernate() {
     }
 
-    @Override
-    public void create(Person createUser) throws Exception{
-        session = sessionFactory.openSession();
-        tx = session.beginTransaction();
-        session.save(createUser);
-        tx.commit();
-        session.close();
+    private Session getCurrentSession()
+    {
+        return sessionFactory.getCurrentSession();
     }
 
     @Override
-    public List<Person> read() throws Exception{
+    public Person create(Person createPerson) throws Exception{
+        //session = sessionFactory.openSession();
+        session = getCurrentSession();
+       // tx = session.beginTransaction();
+        session.save(createPerson);
+        //tx.commit();
+        logger.info("Add entity to db: "+createPerson);
+        //session.close();
+        return createPerson;
+    }
 
-        session = sessionFactory.openSession();
-        tx = session.beginTransaction();
+    @Override
+    @Transactional(readOnly=true)
+    public List<Person> read() throws Exception{
+        //session = sessionFactory.openSession();
+        session = getCurrentSession();
+        //tx = session.beginTransaction();
         List personList = session.createCriteria(Person.class).list();
-        tx.commit();
-        session.close();
+        //tx.commit();
+        //session.close();
+        logger.info("Get entitys[" + Person.class + "]from db:");
         return personList;
     }
 
     @Override
+    @Transactional(readOnly=true)
     public Person read(int id) throws Exception {
-        session = sessionFactory.openSession();
+        //session = sessionFactory.openSession();
+        session = getCurrentSession();
         Person person = (Person) session.load(Person.class, new Integer(id));
-        tx = session.getTransaction();
-        session.beginTransaction();
-        tx.commit();
+        //tx = session.getTransaction();
+        //session.beginTransaction();
+       // tx.commit();
+        logger.info("Get by id: " +id + " entity: "+person);
+        return person;
+    }
+
+
+    @Override
+    public Person update(Person person) {
+        //session = sessionFactory.openSession();
+        session = getCurrentSession();
+        //Person person1 = session.load(Person.class, person.getIdPerson());
+        //tx = session.getTransaction();
+        //person1 = person;
+        //tx = session.beginTransaction();
+        session.saveOrUpdate(person);
+        logger.info("Update entity: "+person);
+        //tx.commit();
+        //session.close();
         return person;
     }
 
     @Override
     public void delete(int id) throws Exception{
-        session = sessionFactory.openSession();
-        Object o = session.load(Person.class, id);
-        tx = session.getTransaction();
-        session.beginTransaction();
+        //session = sessionFactory.openSession();
+        session = getCurrentSession();
+        Person o = session.load(Person.class, id);
+        //tx = session.getTransaction();
+        //session.beginTransaction();
         session.delete(o);
-        tx.commit();
+        //tx.commit();
 
-    }
-
-    @Override
-    public void update(Person person) {
-        session = sessionFactory.openSession();
-        Person person1;
-        person1 = (Person) session.load(Person.class, person.getIdPerson());
-        tx = session.getTransaction();
-        person1 = person;
-        tx = session.beginTransaction();
-        session.saveOrUpdate(person1);
-        tx.commit();
-        session.close();
     }
 
     @Override
