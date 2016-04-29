7551c032e705cf24ab3b9ba83ddd75b8d748901e
lumiandrey <lumiandrey@live.com>
2016-04-30 00:13:19 +0300

    Revert "rating, изменение"
    This reverts commit cef6054dee2a4e2fbfcb53397c05a354cbf4064e.

--- RESTfulAPIKorpPortal/src/main/java/by/bsuir/ief/rest/model/entity/PersonEntity.java
+++ RESTfulAPIKorpPortal/src/main/java/by/bsuir/ief/rest/model/entity/PersonEntity.java
@@ -125,7 +125,7 @@ public class PersonEntity {
     }
 
     @Basic
-    @Column(name = "rating", nullable = false, precision = 0)
+    @Column(name = "reiting", nullable = false, precision = 0)
     public double getReiting() {
         return reiting;
     }
