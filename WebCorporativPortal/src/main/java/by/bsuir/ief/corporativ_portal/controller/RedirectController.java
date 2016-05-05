e216dc21119b1d3c9c5e172bb303c34354c92ede
lumiandrey <lumiandrey@live.com>
2016-05-05 15:18:10 +0300

    Обновление управления механизмами сессии

--- WebCorporativPortal/src/main/java/by/bsuir/ief/corporativ_portal/controller/RedirectController.java
+++ WebCorporativPortal/src/main/java/by/bsuir/ief/corporativ_portal/controller/RedirectController.java
@@ -17,7 +17,17 @@ public class RedirectController {
 
     @RequestMapping(value="/", method = RequestMethod.GET)
     public ModelAndView main(HttpSession session){
-        session.invalidate();
-        return new ModelAndView(ClientURL.getProperty("url.login")/*"index"*/, "user", new User());
+        ModelAndView modelAndView = new ModelAndView();
+        if(session.isNew()) {
+            modelAndView.setViewName(ClientURL.getProperty("url.login"));
+            modelAndView.addObject("user", new User());
+        }
+        else {
+            modelAndView.setViewName(ClientURL.getProperty("url.main"));
+            User user = (User) session.getAttribute("user");
+            //modelAndView.addObject("user", user);
+            modelAndView.addObject("person", user.getPerson());
+        }
+        return modelAndView;
     }
 }
