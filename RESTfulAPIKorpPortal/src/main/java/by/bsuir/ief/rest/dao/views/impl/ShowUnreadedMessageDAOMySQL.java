cb86c2ccf0e6c5a213b1994d878e610c3a66cbb8
lumiandrey <lumiandrey@live.com>
2016-05-06 19:02:29 +0300

    Создание ДАО класса привязанного к определённой БД(MySQL) использующий диалект MySQL и Spring JDBC

---
+++ RESTfulAPIKorpPortal/src/main/java/by/bsuir/ief/rest/dao/views/impl/ShowUnreadedMessageDAOMySQL.java
@@ -0,0 +1,41 @@
+package by.bsuir.ief.rest.dao.views.impl;
+
+import by.bsuir.ief.rest.dao.views.ShowUnreadedMessageDAO;
+import by.bsuir.ief.rest.model.entity.views.ShowUnreadedMessage;
+import by.bsuir.ief.rest.util.DateConvert;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+
+import java.text.ParseException;
+import java.util.List;
+
+/**
+ * Created by andrey on 06.05.2016.
+ */
+@Component
+public class ShowUnreadedMessageDAOMySQL implements ShowUnreadedMessageDAO {
+
+
+    private static String SQL_GET_UNREADED_MESSAGE_BY_ID = "select * from `show_unreaded_message` where `user_rec` = ?";
+    @Qualifier("jdbcTemplate")
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Override
+    public List<ShowUnreadedMessage> readByUserId(int userId) throws Exception {
+        return jdbcTemplate.query(SQL_GET_UNREADED_MESSAGE_BY_ID, (RowMapper<ShowUnreadedMessage>) (rs, rowNum) -> {
+            ShowUnreadedMessage message = new ShowUnreadedMessage();
+            try {
+                message.setDate(DateConvert.StringToUtilDate(rs.getString("date")));
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            message.setContent(rs.getString("content"));
+            message.setLogin(rs.getString("login"));
+            return message;
+        }, userId);
+    }
+}
