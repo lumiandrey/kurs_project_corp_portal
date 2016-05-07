b96ccc358dc6c8dc25d1e0fc0fa02e80810b8634
lumiandrey <lumiandrey@live.com>
2016-05-07 12:55:39 +0300

    Изменение обработки сессий при обаботке запроса на страницу входа

--- WebCorporativPortal/src/main/java/by/bsuir/ief/corporativ_portal/controller/RedirectController.java
+++ WebCorporativPortal/src/main/java/by/bsuir/ief/corporativ_portal/controller/RedirectController.java
@@ -20,19 +20,20 @@ public class RedirectController {
     @RequestMapping(value="/", method = RequestMethod.GET)
     public ModelAndView main(HttpSession session){
         ModelAndView modelAndView = new ModelAndView();
-        if(session.isNew()) {
+        try {
+            if (session.isNew()) {
+                modelAndView.setViewName(ClientURL.getProperty("url.login"));
+                modelAndView.addObject("user", new User());
+            } else {
+                modelAndView.setViewName(ClientURL.getProperty("url.main"));
+                User user = (User) session.getAttribute("user");
+                modelAndView.addObject("user", user);
+                modelAndView.addObject("person", user.getPerson());
+            }
+        }catch (Exception e){
+            session.invalidate();
             modelAndView.setViewName(ClientURL.getProperty("url.login"));
             modelAndView.addObject("user", new User());
-        }
-        else {
-            modelAndView.setViewName(ClientURL.getProperty("url.main"));
-            User user = (User) session.getAttribute("user");
-            modelAndView.addObject("user", user);
-            modelAndView.addObject("person", user.getPerson());
-            modelAndView.addObject("department", user.getPerson().getDepatment());
-            modelAndView.addObject("post", user.getPerson().getPost());
-            modelAndView.addObject("city", user.getPerson().getCity());
-            modelAndView.addObject("country", user.getPerson().getCity().getCountry());
         }
         return modelAndView;
     }
