905bfde9548f0a0324ec501d30998da16f923543
DariaSSemikina <dashulya_plyus@mail.ru>
2016-05-07 03:23:04 +0300

    Переписка с человеком (+скрипты для обноыления 2-х таблиц базы!)

---
+++ dump/message_receiver.sql
@@ -0,0 +1,17 @@
+/*
+-- Query: SELECT * FROM korporativ_portal.message_receiver
+LIMIT 0, 1000
+
+-- Date: 2016-05-07 03:20
+*/
+INSERT INTO `message_receiver` (`id_message`,`id_user_receiver`) VALUES (17,3);
+INSERT INTO `message_receiver` (`id_message`,`id_user_receiver`) VALUES (21,5);
+INSERT INTO `message_receiver` (`id_message`,`id_user_receiver`) VALUES (22,3);
+INSERT INTO `message_receiver` (`id_message`,`id_user_receiver`) VALUES (23,3);
+INSERT INTO `message_receiver` (`id_message`,`id_user_receiver`) VALUES (25,3);
+INSERT INTO `message_receiver` (`id_message`,`id_user_receiver`) VALUES (26,3);
+INSERT INTO `message_receiver` (`id_message`,`id_user_receiver`) VALUES (27,3);
+INSERT INTO `message_receiver` (`id_message`,`id_user_receiver`) VALUES (19,4);
+INSERT INTO `message_receiver` (`id_message`,`id_user_receiver`) VALUES (18,5);
+INSERT INTO `message_receiver` (`id_message`,`id_user_receiver`) VALUES (20,5);
+INSERT INTO `message_receiver` (`id_message`,`id_user_receiver`) VALUES (24,5);
