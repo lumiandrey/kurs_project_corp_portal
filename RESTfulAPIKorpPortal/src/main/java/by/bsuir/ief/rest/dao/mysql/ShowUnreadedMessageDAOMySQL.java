740d6f7dd5c3dca61550e21ff5849d1874c5830c
SpongeIsNotBoB <karachunvladislav@Gmail.com>
2016-05-08 18:01:38 +0300

    Обновление вставки индексов в множестенную таблицу

--- RESTfulAPIKorpPortal/src/main/java/by/bsuir/ief/rest/dao/mysql/ShowUnreadedMessageDAOMySQL.java
+++ RESTfulAPIKorpPortal/src/main/java/by/bsuir/ief/rest/dao/mysql/ShowUnreadedMessageDAOMySQL.java
@@ -6,8 +6,11 @@ import by.bsuir.ief.rest.util.DateConvert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.text.ParseException;
 import java.util.List;
 
@@ -42,31 +45,26 @@ public class ShowUnreadedMessageDAOMySQL implements ShowUnreadedMessageDAO {
             "on `rt`.`sender_id` = `u1`.`id_user` " +
             "order by `rt`.`date` ";
 
+    private static String SQL_INSERT_INTO_MESSAGE_RECIVER = "insert into message_receiver (id_message, id_user_receiver) values (?, ?)";
+
     @Qualifier("jdbcTemplate")
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
     @Override
     public List<ShowUnreadedMessage> readByUserId(int userId) throws Exception {
-        return jdbcTemplate.query(SQL_GET_UNREADED_MESSAGE_BY_ID, (rs, rowNum) -> {
-            ShowUnreadedMessage message = new ShowUnreadedMessage();
-            try {
-                message.setDate(DateConvert.StringToUtilDate(rs.getString("date")));
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
-            message.setContent(rs.getString("content"));
-            message.setLogin(rs.getString("login"));
-            message.setUserRec(rs.getInt("user_rec"));
-            message.setUserSender(rs.getInt("sender_id"));
-            return message;
-        }, userId);
+        return jdbcTemplate.query(SQL_GET_UNREADED_MESSAGE_BY_ID, new ShowUnreadedMessageMapper(), userId);
 
     }
 
     @Override
     public List<ShowUnreadedMessage> readMessagesByIdSender(int idSenser, int idReciver) throws Exception {
-        return jdbcTemplate.query(SQL_GET_MESSAGE_ONE_SENDER, (rs, rowNum) -> {
+        return jdbcTemplate.query(SQL_GET_MESSAGE_ONE_SENDER, new ShowUnreadedMessageMapper(), idReciver, idSenser,idReciver,idSenser);
+    }
+
+    private static final class ShowUnreadedMessageMapper implements RowMapper<ShowUnreadedMessage> {
+
+        public ShowUnreadedMessage mapRow(ResultSet rs, int rowNum) throws SQLException {
             ShowUnreadedMessage message = new ShowUnreadedMessage();
             try {
                 message.setDate(DateConvert.StringToUtilDate(rs.getString("date")));
@@ -78,7 +76,13 @@ public class ShowUnreadedMessageDAOMySQL implements ShowUnreadedMessageDAO {
             message.setUserRec(rs.getInt("user_rec"));
             message.setUserSender(rs.getInt("sender_id"));
             return message;
-        }, idReciver, idSenser,idReciver,idSenser);
+        }
+    }
+
+    @Override
+    public int insertMessageReciverTable(int idMessage, int idReciver)throws Exception
+    {
+        return jdbcTemplate.update(SQL_INSERT_INTO_MESSAGE_RECIVER, idMessage, idReciver);
     }
 
 
